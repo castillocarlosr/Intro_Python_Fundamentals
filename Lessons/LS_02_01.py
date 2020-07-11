b4ad367d920ca7d70b68d162ed245dc5e47f11fa
castillocarlosr <castillocarlosr@gmail.com>
2020-07-10 17:09:46 -0700

    simple input print functions

---
+++ Lessons/LS_02_01.py
@@ -0,0 +1,7 @@
+name = input("Hi, what's your name? ")
+age = int(input("How old are you? "))
+
+if (age < 18):
+    print("You're too young to fly", name)
+else:
+    print("Feel free to join space force", name)  
