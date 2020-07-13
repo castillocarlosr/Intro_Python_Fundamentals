8f5335008070829792621e3445c95bf0da7d339c
castillocarlosr <castillocarlosr@gmail.com>
2020-07-13 11:50:49 -0700

    added 2nd file more examples

--- Lessons/LS_03_01.py
+++ Lessons/LS_03_01.py
@@ -3,7 +3,7 @@ print(age)
 print(type(age))
 
 
-email_address = "john.doe@me.com"
+email_address = "john.doh@me.com"
 print(email_address)
 print(type(email_address))
 
---
+++ Lessons/LS_03_02.py
@@ -0,0 +1,26 @@
+print('-------------')
+
+cookies = 'chocolate'
+print(cookies)
+print(type(cookies))
+print('-------------')
+
+cookies = 2
+print(cookies)
+print(type(cookies))
+print('-------------')
+
+Cookies = 3
+print(Cookies)
+print(cookies)
+print('-------------')
+
+first_name = "Vanessa"
+print(first_name)
+print('-------------')
+
+first_Name = "Brian"
+print(first_name)
+print(first_Name)
+
+print('-------------')
