52d5fa7776d655dc7ee9e96624dab07db756302b
castillocarlosr <castillocarlosr@gmail.com>
2020-07-15 11:04:16 -0700

    added new if statements

---
+++ Lessons/LS_04_01.py
@@ -0,0 +1,17 @@
+plant = "Cacti"
+
+if plant == "Cacti":
+    print(plant, "don't need a lot of water")
+else:
+    print(plant, "love water")
+
+print("Thanks!")
+
+plantTwo = "Irises"
+
+if plantTwo == "Cacti":
+    print(plantTwo, "don't need a lot of water")
+else:
+    print(plantTwo, "love water")
+
+print("Thanks again!")
